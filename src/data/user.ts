1d85be41b49d6a41a2341c119aec6d18d83d6c54
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-09-24 14:47:37 -0300

    Criando user fake

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'claudine',
+    name: 'Claudine Roberto',
+    avatar: 'https://ui-avatars.com/api/?name=Claudine=Roberto&background=random&size=100',
+    cover: 'https://br.freepik.com/fotos-gratis/bela-praia-tropical-e-mar-com-cadeira-no-ceu-azul_4188236.htm#query=praia&position=4&from_view=keyword&track=ais_hybrid&uuid=47d47a1c-feae-4f7b-9cc0-0e67bba87c94',
+    bio: 'Desenvolvedor Front-end',
+    link: 'https://claudineroberto.com.br',
+    postCount: 42,
+};
