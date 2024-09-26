6d8a4cdef25f68d18c32fcb97b04664820b590bc
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2024-09-26 13:11:54 -0300

    Criando pagina de perfil

--- src/data/user.ts
+++ src/data/user.ts
@@ -4,7 +4,7 @@ export const user: User = {
     slug: 'claudine',
     name: 'Claudine Roberto',
     avatar: 'https://ui-avatars.com/api/?name=Claudine=Roberto&background=random&size=100',
-    cover: 'https://br.freepik.com/fotos-gratis/bela-praia-tropical-e-mar-com-cadeira-no-ceu-azul_4188236.htm#query=praia&position=4&from_view=keyword&track=ais_hybrid&uuid=47d47a1c-feae-4f7b-9cc0-0e67bba87c94',
+    cover: 'https://www.civitatis.com/blog/wp-content/uploads/2023/12/shutterstock_318248558-1920x1280.jpg',
     bio: 'Desenvolvedor Front-end',
     link: 'https://claudineroberto.com.br',
     postCount: 42,
